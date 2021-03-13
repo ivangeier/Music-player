b3b6d410f69a7b64298cf06bb4fe11054ed5d860
vinicius-je <vinicius.je99@gmail.com>
2021-03-13 10:04:18 -0300

    Update

--- musicPlayer.js
+++ musicPlayer.js
@@ -7,7 +7,7 @@ let display = document.getElementById("timerDisplay")
 let volume = document.getElementById("volume")
 let music = document.getElementById("music");
 
-/* Start music */
+// Start music
 function play(){
     music.play();
     start_btn.style.display="none";
@@ -16,24 +16,24 @@ function play(){
     time();
 }
 
-/* Pause music */
+// Pause music
 function pause(){
     music.pause();
     start_btn.style.display="block";
     stop_btn.style.display="none";
 }
 
-/* forward music 10 seconds */
+// forward music 10 seconds
 function forward(){
     music.currentTime += 10;
 }
 
-/* Backward music 10 seconds */
+// Backward music 10 seconds
 function backward(){
     music.currentTime -= 10;
 }
 
-/* Start music again */
+// Start music again
 function again(){
     music.currentTime = 0;
 }
@@ -42,7 +42,7 @@ function musicVolume(value){
     music.volume = value;
 }
 
-/* Progress time bar */
+// Progress time bar
 function progressBar(){
     timerBar.setAttribute('value', music.currentTime);
     timerBar.setAttribute('max', music.duration);
@@ -50,7 +50,7 @@ function progressBar(){
 
 setInterval(progressBar, 1000)
 
-/* Fourward/backward music and progress bar */
+// Fourward/backward music and progress bar
 timerBar.addEventListener("input", function(event){
     music.currentTime = event.target.value;
     music.play()
@@ -63,26 +63,26 @@ timerBar.addEventListener("input", function(event){
     setInterval(resetProgressBar, 1000)
 })
 
-/* Music time */
+// Music time
 function time(){
 
     let time = music.duration - music.currentTime;
 
-    let horas = Math.round(time / 3600);
-    let minutos = Math.floor((time - (horas * 3600)) / 60);
-    let segundos = Math.round(time % 60);
+    let hour = Math.round(time / 3600);
+    let minutes = Math.floor((time - (hour * 3600)) / 60);
+    let seconds = Math.round(time % 60);
 
-    if(horas < 10){
-        horas = "0" + horas;
+    if(hour < 10){
+        hour = "0" + hour;
     }
-    if (minutos < 10) {
-        minutos = "0" + minutos
+    if (minutes < 10) {
+        minutes = "0" + minutes;
     }
-    if (segundos < 10) {
-        segundos = "0" + segundos;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
     }
 
-    display.innerText = horas + ":" + minutos + ":" + segundos;
+    display.innerText = hour + ":" + minutes + ":" + seconds;
 }
 
 setInterval(time, 1000);
